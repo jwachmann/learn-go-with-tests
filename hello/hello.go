c92691fa446c3e6ebff709be7822a993136e825f
jwachmann <james.wachmann@gmail.com>
2021-01-12 20:02:31 -0500

    Hello World module complete

--- hello/hello.go
+++ hello/hello.go
@@ -6,13 +6,37 @@ import (
 	"rsc.io/quote"
 )
 
+const french = "French"
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello"
+const frenchHelloPrefix = "Bonjour"
+const spanishHelloPrefix = "Hola"
+
 func main() {
-	fmt.Println(hello("World"))
+	fmt.Println(hello("World", ""))
 	fmt.Println(rscHello())
 }
 
-func hello(name string) string {
-	return fmt.Sprintf("Hello %v!", name)
+func hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := greetingPrefix(language)
+
+	return fmt.Sprintf("%v %v!", prefix, name)
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func rscHello() string {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,18 +5,40 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	want := "Hello James!"
-	got := hello("James")
 
-	if got != want {
-		t.Errorf("Got %q, want %q", got, want)
-	}
+	t.Run("Saying hello to people", func(t *testing.T) {
+		want := "Hello James!"
+		got := hello("James", "")
+		assertMatch(t, got, want)
+	})
+
+	t.Run("Say 'Hello World!' when an empty string is supplied", func(t *testing.T) {
+		want := "Hello World!"
+		got := hello("", "")
+		assertMatch(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		want := "Hola Dora!"
+		got := hello("Dora", "Spanish")
+		assertMatch(t, got, want)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		want := "Bonjour Phileas!"
+		got := hello("Phileas", "French")
+		assertMatch(t, got, want)
+	})
 }
 
 func TestRscHello(t *testing.T) {
 	want := "Hello, world."
 	got := rscHello()
+	assertMatch(t, got, want)
+}
 
+func assertMatch(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Got %q, want %q", got, want)
 	}
